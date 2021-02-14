ca1536b3ed8ac086967cc3229fdd92228e5eba9d
Angel Cochachez <acochachez@gmail.com>
2021-02-13 23:59:23 -0800

    Added shoes selector

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Image, TouchableOpacity} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
 
 //Screens
 import {Home} from './screens/';
+import {icons, COLORS, FONTS, SIZES} from './constants';
 
 const theme = {
   ...DefaultTheme,
@@ -22,7 +24,32 @@ const App = () => {
         <Stack.Screen
           name="Home"
           component={Home}
-          options={{title: 'SHOE SELECTOR'}}
+          options={{
+            title: 'SHOE SELECTOR',
+            headerTintColor: COLORS.lightGray,
+            headerTitleStyle: {...FONTS.navTitle},
+            headerLeft:({onPress})=>(
+              <TouchableOpacity style={{marginleft:SIZES.padding}}
+              onPress={onPress}
+              >
+                <Image source={icons.menu}
+                resizeMode="contain"
+                style={{width:25, height:25}}}
+
+                />
+
+              </TouchableOpacity>
+            ),
+            headerRight:()=>(
+              <TouchableOpacity style={{marginRight:SIZES.padding}}
+              onPress={()=>console.log("Header right on pressed")}>
+              <Image
+              source={icons.search}
+              resize
+              </TouchableOpacity>
+
+            )
+          }}
         />
       </Stack.Navigator>
     </NavigationContainer>
